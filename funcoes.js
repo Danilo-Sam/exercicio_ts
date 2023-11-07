d69631bd8239ce257eef9b549d06992a8a6b3e8e
Danilo-Sam <danilobrown17@gmail.com>
2023-11-07 19:55:26 -0300

    finalização do exercicio de typescript

---
+++ funcoes.js
@@ -0,0 +1,14 @@
+"use strict";
+function multiplicar(numA, numB) {
+    const res = numA * numB;
+    return res;
+}
+const n1 = 9;
+const n2 = 7;
+const resultado = multiplicar(n1, n2);
+console.log(resultado);
+const nome = "Danilo Sampaio";
+function saldacao() {
+    return "Olá " + nome;
+}
+console.log(saldacao());
---
+++ funcoes.ts
@@ -0,0 +1,19 @@
+function multiplicar(numA : number, numB : number): number {
+    const res: number = numA * numB
+    return res;
+}
+
+const n1: number = 9;
+const n2: number = 7;
+
+const resultado: number = multiplicar(n1, n2)
+
+console.log(resultado);
+
+const nome: string = "Danilo Sampaio";
+
+function saldacao(): string {
+    return "Olá " + nome;
+}
+
+console.log(saldacao());
